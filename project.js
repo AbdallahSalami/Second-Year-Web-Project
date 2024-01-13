69db1c087c78e9f713c5ada25efff666e162c082
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2024-01-13 23:45:49 +0200

    no need

---
+++ project.js
@@ -0,0 +1,10 @@
+var clickedNumber = document.getElementById('one');
+
+clickedNumber.addEventListener('click', () => {
+    alert('1')
+
+} )
+
+
+
+var clickedAnyNumber = document.getElementsByClassName()
